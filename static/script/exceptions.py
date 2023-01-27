bd1701fb722f05ee5be19ebf1bc217f39db06a64
evzen@p1463k <p1463k@gmail.com>
2023-01-27 09:45:22 +0100

    Added 422 exception handler

--- static/script/exceptions.py
+++ static/script/exceptions.py
@@ -7,27 +7,43 @@ templates = Jinja2Templates(directory="templates")
 
 async def not_found_error(request: Request, exc: HTTPException):
 
-    return templates.TemplateResponse(
-        "errors/404.html", {"request": request}, status_code=404
-    )
+    context = {"detail": str(exc.detail), "request": request}
+
+    return templates.TemplateResponse("errors/404.html", context, status_code=404)
 
 
 async def internal_error(request: Request, exc: HTTPException):
 
-    return templates.TemplateResponse(
-        "errors/500.html", {"request": request}, status_code=500
-    )
+    context = {"request": request}
+
+    return templates.TemplateResponse("errors/500.html", context, status_code=500)
 
 
 async def expired_token_error(request: Request, exc: HTTPException):
 
-    return templates.TemplateResponse(
-        "errors/403.html", {"request": request}, status_code=403
-    )
+    context = {"detail": str(exc.detail), "request": request}
+
+    return templates.TemplateResponse("errors/403.html", context, status_code=403)
+
+
+async def unauthorized_error(request: Request, exc: HTTPException):
+
+    context = {"detail": str(exc.detail), "request": request}
+
+    return templates.TemplateResponse("errors/401.html", context, status_code=401)
+
+
+async def unprocessable_entity_error(request: Request, exc: HTTPException):
+
+    context = {"detail": str(exc.detail), "request": request}
+
+    return templates.TemplateResponse("errors/422.html", context, status_code=422)
 
 
 exception_handlers = {
     404: not_found_error,
     500: internal_error,
     403: expired_token_error,
+    401: unauthorized_error,
+    422: unprocessable_entity_error,
 }
